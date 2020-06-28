d9d2a9228edac93662740b374d49515557a3099a
Teeh Amaral <contato@teehamaral.me>
2020-06-28 18:50:07 -0300

    Add docker config

---
+++ docker/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+case $1 in
+    supervisor)
+        python3.6 manage.py compress --extension=.haml --force
+        python3.6 docker/clear-compressor-cache.py
+        python3.6 manage.py migrate --noinput
+        /usr/local/bin/supervisord -n -c docker/supervisor-app.conf
+    ;;
+
+esac
+
+exec "$@"
