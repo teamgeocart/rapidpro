85dbd24b05f21a4c83f690e7930784c0e1186f2e
Teeh Amaral <contato@teehamaral.me>
2020-06-28 18:49:50 -0300

    Courier TPS on settings file

--- temba/settings_common.py
+++ temba/settings_common.py
@@ -1133,3 +1133,5 @@ ELASTICSEARCH_URL = os.environ.get("ELASTICSEARCH_URL", "http://localhost:9200")
 # Maximum active objects are org can have
 MAX_ACTIVE_CONTACTFIELDS_PER_ORG = 255
 MAX_ACTIVE_GLOBALS_PER_ORG = 255
+
+COURIER_DEFAULT_TPS = int(os.environ.get("COURIER_DEFAULT_TPS", 100))
